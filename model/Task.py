7d5b0931623256cf0191fe4d5a2802ae766bcb91
thitirat1 <thitirat@code-app.com>
2020-03-28 23:15:16 +0700

    database class account

--- model/Task.py
+++ model/Task.py
@@ -1,4 +1,6 @@
 from typing import List
+
+
 class Task:
     def __init__(self, title: str):
         self.__title = title
@@ -7,38 +9,36 @@ class Task:
         self.__responsibleMemberUsernames = []
         self.__comments = []
         self.__tags = []
-    
+
     def getTitle(self) -> str:
         return self.__title
-    
+
     def getResponsibleMemberUsernames(self) -> List[str]:
         return self.__responsibleMemberUsernames
-    
+
     def getDueDate(self) -> str:
         return self.__dueDate
-    
+
     def getComments(self) -> List[str]:
         return self.__comments
-    
+
     def getTags(self) -> List[str]:
         return self.__tags
-    
+
     def addTag(self, tag: str):
         self.__tags.append(tag)
-        
+
     def removeTag(self, tag: str):
         self.__tags.remove(tag)
-        
+
     def addComment(self, comment: str):
         self.__comments.append(comment)
-        
+
     def addResponsibleMemberUsername(self, username: str):
         self.__responsibleMemberUsernames.append(username)
-        
+
     def addDueDate(self, date: str):
         self.__dueDate = date
-    
+
     def removeDueDate(self):
         self.__dueDate = None
-    
-        
