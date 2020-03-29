0ea86cd067217f7f50e7ffc423b69c8353d6b2ef
thitirat1 <thitirat@code-app.com>
2020-03-29 23:56:03 +0700

    refactor board db class to snake case

---
+++ database_model/Board.py
@@ -0,0 +1,13 @@
+from mongoengine import *
+
+
+class Board(Document):
+    board_id = IntField(unique=True)
+    board_title = StringField(required=True)
+    section_title = ListField(StringField())
+    members = ListField(StringField())
+
+    meta = {
+        "collection": "board",
+        "ordering": ["-date_created"]
+    }
