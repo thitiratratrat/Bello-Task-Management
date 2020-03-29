841364ab266af477d309130c1261af23c18f7f5e
Thitiratratrat <altothiti@gmail.com>
2020-03-29 23:29:18 +0700

    added boardId to Account database class

--- database_model/Account.py
+++ database_model/Account.py
@@ -4,5 +4,6 @@ from mongoengine import *
 class Account(Document):
     username = StringField(required=True)
     password = StringField(required=True)
+    boardId = ListField(IntField(), default=list)
 
     meta = {'collection': 'account'}
