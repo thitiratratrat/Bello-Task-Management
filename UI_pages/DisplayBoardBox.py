168f7f4719fa651efb1fd2604ac23bd45f9e3684
Thitiratratrat <altothiti@gmail.com>
2020-04-02 17:57:42 +0700

    separated classes

---
+++ UI_pages/DisplayBoardBox.py
@@ -0,0 +1,32 @@
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from random import randint
+
+
+class DisplayBoardBox(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.listWidget = QListWidget()
+        self.listWidget.setViewMode(QListWidget.IconMode)
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.listWidget)
+        self.listWidget.setIconSize(QSize(200, 80))
+        self.listWidget.setFont(QFont("Moon", 10))
+        self.board1 = QListWidgetItem(QIcon(self.createBox()), "Bello")
+        self.listWidget.insertItem(1, self.board1)
+        self.setLayout(self.layout)
+        self.i = 1
+
+    def createBox(self):
+        self.ran_num1 = randint(0, 255)
+        self.ran_num2 = randint(0, 255)
+        self.ran_num3 = randint(0, 255)
+        self.recPainter = QPixmap(120, 80)
+        self.recPainter.fill(
+            QColor(self.ran_num1, self.ran_num2, self.ran_num3))
+        return self.recPainter
+
+    def addToListWidget(self, board):
+        self.i += 1
+        self.listWidget.insertItem(self.i, board)
