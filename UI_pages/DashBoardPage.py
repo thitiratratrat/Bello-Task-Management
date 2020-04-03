633a54399ce5314a45c3f972812d3069917a3d6c
candyccyn <61090004@kmitl.ac.th>
2020-04-03 13:07:06 +0700

    edit dashboard page layout

--- UI_pages/DashBoardPage.py
+++ UI_pages/DashBoardPage.py
@@ -6,61 +6,83 @@ from random import randint
 from TabBar import * 
 from MenuBar import * 
 
+class TextOverDelegate(QStyledItemDelegate):
+    def __init__(self, parent=None):
+        super(TextOverDelegate, self).__init__(parent)
+    def paint(self, painter, option, index):
+        super(TextOverDelegate,self).paint(painter,option,index)
+        text = index.data(QtCore.Qt.UserRole)
+        if(option.widget):
+            style = option.widget.style()
+        else:
+            style = QApplication.style()
+        style.drawItemText(painter, option.rect,QtCore.Qt.AlignCenter, option.palette, option.state & QStyle.State_Enabled, text)
+
 class DisplayBoardBox(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
+        self.setFixedSize(480,350)
         self.listWidget = QListWidget()
+        self.listWidget.setSpacing(10)
+        self.listWidget.setMovement(QListView.Static)
         self.listWidget.setViewMode(QListWidget.IconMode)
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.listWidget)
         self.listWidget.setIconSize(QSize(200,80))
         self.listWidget.setFont(QFont("Moon",10))
-        self.board1 = QListWidgetItem(QIcon(self.createBox()),"Bello")
-        self.listWidget.insertItem(1,self.board1)
         self.setLayout(self.layout)
         self.i = 1
-    def createBox(self):
-        self.ran_num1 = randint(0,255)
-        self.ran_num2 = randint(0,255)
-        self.ran_num3 = randint(0,255)
-        self.recPainter = QPixmap(120, 80)
-        self.recPainter.fill(QColor(self.ran_num1,self.ran_num2,self.ran_num3))
-        return self.recPainter
+    def createBox(self,text):
+        self.ran_num1 = randint(0,150)
+        self.ran_num2 = randint(0,199)
+        self.ran_num3 = randint(0,226)
+        self.board1 = QListWidgetItem()
+        self.board1.setBackground(QColor(self.ran_num1,self.ran_num2,self.ran_num3))
+        self.board1.setSizeHint(QSize(120, 80))
+        self.board1.setFont(QFont("Century Gothic", 12, QFont.Bold))
+        self.board1.setTextColor("white")
+        self.board1.setTextAlignment(Qt.AlignLeft)
+        self.board1.setText(text)
+        self.addToListWidget(self.board1)
     def addToListWidget(self,board):
         self.i += 1 
         self.listWidget.insertItem(self.i,board)
 
 class DashboardPage(QWidget):
-    def __init__(self,parent):
+    def __init__(self,parent=None):
         super(DashboardPage,self).__init__(parent)
         self.parent = parent
-        self.createBtn = QPushButton()
         self.menuBar = MenuBar()
+        self.menuBar.move(QPoint(0,0))
         self.tabBarBoard = TabWidget()
+        self.tabBarBoard.setFixedSize(585,355)
         self.tabBarBoard.setFont(QFont("Moon",10,QFont.Bold))
         self.displayBoard = DisplayBoardBox()
-        self.addBoardBtn = QPushButton("+ ADD board")
+        self.addBoardBtn = QPushButton("Add")
+        self.addBoardBtn.setIcon(QIcon("images/add.png"))
+        self.addBoardBtn.setStyleSheet("background-color:rgb(14,172,120);color:rgb(255,255,255)")
+        self.addBoardBtn.setFont(QFont("Century Gothic",8,QFont.Bold))
         self.addBoardBtn.clicked.connect(self.createNewBoard)
-        self.deleteBoardBtn = QPushButton("- Delete board")
+        self.deleteBoardBtn = QPushButton("Delete")
+        self.deleteBoardBtn.setIcon(QIcon("images/delete.png"))
+        self.deleteBoardBtn.setStyleSheet("background-color:rgb(210,39,62);color:rgb(255,255,255)")
+        self.deleteBoardBtn.setFont(QFont("Century Gothic",8,QFont.Bold))
         self.deleteBoardBtn.clicked.connect(self.deleteSelectBoard)
         self.tabBarBoard.addTab(self.displayBoard, QIcon("images/boardIcon.png"), " Board")
-        self.tabBarBoard.addTab(QWidget(),"Team")
         self.layout = QGridLayout()
-        self.menuBar.setContentsMargins(0,0,0,20)
-        self.layout.addWidget(self.menuBar,0,0)
+        self.menuBar.setContentsMargins(0,0,0,0)
+        self.layout.addWidget(self.menuBar, 0,0 )
+        #self.layout.addLayout(self.menubar_widget,0,0)
         self.tabBarBoard.setContentsMargins(10,0,30,0)
         self.layout.addWidget(self.tabBarBoard,1,0,1,2)
-        self.layout.addWidget(self.addBoardBtn,2,1,1,2)
-        self.layout.addWidget(self.deleteBoardBtn,2,2,1,2)
+        self.btnLayout = QHBoxLayout()
+        self.btnLayout.addStretch(1)
+        self.btnLayout.addWidget(self.addBoardBtn)
+        self.btnLayout.addWidget(self.deleteBoardBtn)
+        self.layout.addLayout(self.btnLayout,2,0)
         self.setLayout(self.layout)
         self.show()
-    def paintEvent(self,e):
-        paint = QPainter()
-        paint.begin(self)
-        paint.setPen(QColor(61,59,59))
-        paint.setBrush(QColor(61,59,59))
-        paint.drawRect(0,0, 640, 48)
-        paint.end()
+    
     def createNewBoard(self):
         self.createBoardDialog = QDialog(self)
         self.createBoardDialog.setWindowTitle("Create board")
@@ -76,18 +98,18 @@ class DashboardPage(QWidget):
         self.createBtn.setFont(QFont("Moon",10))
         self.createBtn.clicked.connect(self.createBtnAddBoard)
         self.createBoardDialog.show()
-    
+
     def getBoardName(self):
         return self.boardNameValue.text()
     def addBoard(self,boardName):
         self.board = QListWidgetItem(QIcon(self.displayBoard.createBox()), boardName)
         self.displayBoard.addToListWidget(self.board)
+        
     def createBtnAddBoard(self):
         boardName = self.getBoardName()
-        self.board = QListWidgetItem(QIcon(self.displayBoard.createBox()), boardName)
-        self.displayBoard.addToListWidget(self.board)
+        self.displayBoard.createBox(boardName)
+        self.displayBoard.setContentsMargins(10,10,10,10)
         self.closeDialog()
-
     def closeDialog(self):
         self.createBoardDialog.reject()
     def deleteSelectBoard(self):
@@ -99,7 +121,7 @@ class DashboardPage(QWidget):
             self.displayBoard.listWidget.takeItem(self.displayBoard.listWidget.row(item))
 def main():
     app = QApplication(sys.argv)
-    w = Dashboard_Page()
+    w = DashboardPage()
     w.resize(640, 480)
     w.show()
     return app.exec_()
