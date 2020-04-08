f41f369759da5e07c42c910758cf399f82c09082
candyccyn <61090004@kmitl.ac.th>
2020-04-08 21:04:25 +0700

    add edit/create section in BelloUI

--- UI_pages/BoardDetailPage.py
+++ UI_pages/BoardDetailPage.py
@@ -4,74 +4,22 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from MenuBar import *
 from dialogBox import *  
-import random
-
-class TaskWidget(QWidget):
-    def __init__(self):
-        QWidget.__init__(self, None)
-
-
-class Section(QListWidget): # addtask is here
-    def __init__(self):
-        QListWidget.__init__(self, None)
-        self.setFixedSize(200, 480)
-
-
-class SectionWidget(QWidget):
-    def __init__(self, parent=None):
-        super(SectionWidget, self).__init__(None)
-        self.section = Section() #QListWidget
-        self.colorLst = ['#9D9797','#52719F','#31446F']
-        self.setColorSection = random.choice(self.colorLst)
-
-        self.sectionTitleLayout = QHBoxLayout()
-        self.sectionTitle = QLabel(" Sectioname")
-        self.sectionTitle.setStyleSheet("color:white")
-
-        self.editSectionTitleBtn = QToolButton()
-        self.editSectionTitleBtn.setStyleSheet("background-color:rgb(250,231,110)")
-        self.editSectionTitleBtn.setIcon(QIcon('images/edit.png'))
-        self.editSectionTitleBtn.clicked.connect(self.showCreateSection)
-        
-        self.sectionTitle.setFont(QFont("Century Gothic",8,QFont.Bold))
-        self.sectionTitleLayout.addWidget(self.sectionTitle)
-        self.sectionTitleLayout.addWidget(self.editSectionTitleBtn)
-
-        self.mainSectionLayout = QVBoxLayout()
-        self.mainSectionLayout.addLayout(self.sectionTitleLayout)
-        self.mainSectionLayout.addWidget(self.section)
-        self.setColor(self.setColorSection)
-        self.setLayout(self.mainSectionLayout)
-
-    def showCreateSection(self):
-        self.newTitleAndDialogBox = createAddDialog(self,'Edit section title','Section name: ', 'Save',self.editTitleBtnFunc)
-
-    def editTitleBtnFunc(self):
-        self.newSectionTitle = self.newTitleAndDialogBox[0].text()
-        self.editTitle(self.newSectionTitle)
-        self.closeDialogBox()
-
-    def editTitle(self,sectionTitle):
-        self.sectionTitle.setText(sectionTitle)
-
-    def closeDialogBox(self):
-        self.newTitleAndDialogBox[1].reject()
-
-    def setColor(self,colorName):
-        self.palette = QPalette()
-        self.setAutoFillBackground(True)
-        self.palette.setColor(QPalette.Window, QColor('#52719F'))
-        self.setPalette(self.palette)    
+from SectionWidget import * 
 
 class BoardDetailPage(QWidget):
-    def __init__(self):
-        QWidget.__init__(self, None)
+    def __init__(self, parent=None):
+        super(BoardDetailPage, self).__init__(parent)
+        self.parent = parent
+        self.boardId = None
         self.menuBar = MenuBar()
         self.sectionLayout = QHBoxLayout()
         self.widget = QWidget()
+        '''
         for i in range(5):
             self.sectionWidget = SectionWidget()
-            self.sectionLayout.addWidget(self.sectionWidget)
+            self.sectionWidget.setIndexSection(i)
+            self.sectionLayout.addWidget(self.sectionWidget)'''
+
         self.addSectionBtn = QPushButton("Add section")
         self.addSectionBtn.setIcon(QIcon('images/add1.png'))
         self.addSectionBtn.setStyleSheet("background-color: rgb(250,231,111); color: rgb(49,68,111)")
@@ -101,7 +49,14 @@ class BoardDetailPage(QWidget):
         self.boardDetailLayout.addLayout(self.sectionAndAddBtnLayout)
         self.setLayout(self.boardDetailLayout)
     
-    def crateNewSectionToBoard(self):
+    def setBoardId(self, boardId):
+        self.boardId = boardId
+
+    def getBoardId(self):
+        return self.boardId
+
+    def crateNewSectionToBoard(self,boardID):
+        self.setBoardId(boardID)
         self.newSectionWidget = createAddDialog(self,"create new section","Section name:","Create",self.addSectionToWidget)
 
     def getSectionNameFromDialog(self):
@@ -112,18 +67,28 @@ class BoardDetailPage(QWidget):
         self.sectionWidget = SectionWidget()
         self.sectionWidget.editTitle(self.sectionTitileFromDialog)
         self.sectionLayout.addWidget(self.sectionWidget)
-        self.closeDialogBox()
     
     def deleteSectionFromBoard(self):
         self.selectedSectionToDelete = createAddDialog(self,"delete section","Section name:","Delete",self.deleteSection)
     
     def deleteSection(self):
-        print("delete")
-
-
+        isDelete = False
+        for i in range(self.sectionLayout.count()):
+            if(self.sectionLayout.itemAt(i).widget().getSectionTitle() == self.selectedSectionToDelete[0].text()):
+                self.sectionLayout.takeAt(i)
+                isDelete = True
+                self.closeDeleteSectionBox()
+                return isDelete
+            else:
+                continue
+        if(not isDelete):
+            createErrorDialogBox(self,"Error","This section title doesn't exist")
 
     def closeDialogBox(self):
         self.newSectionWidget[1].reject()
+
+    def closeDeleteSectionBox(self):
+        self.selectedSectionToDelete[1].reject()
 
 def main():
     app = QApplication(sys.argv)
@@ -131,7 +96,6 @@ def main():
     w.resize(640, 480)
     w.show()
     return app.exec_()
-
 
 if __name__ == "__main__":
     sys.exit(main())
---
+++ UI_pages/SectionWidget.py
@@ -0,0 +1,64 @@
+import sys
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from Section import *
+from dialogBox import * 
+import random
+
+class SectionWidget(QWidget):
+    def __init__(self, parent=None):
+        super(SectionWidget, self).__init__(None)
+        self.section = Section() #QListWidget
+        self.colorLst = ['#9D9797','#52719F','#31446F']
+        self.setColorSection = random.choice(self.colorLst)
+        self.sectionTitleLayout = QHBoxLayout()
+        self.sectionTitle = QLabel(" Sectioname")
+        self.sectionTitle.setStyleSheet("color:white")
+        self.editSectionTitleBtn = QToolButton()
+        self.editSectionTitleBtn.setStyleSheet("background-color:rgb(250,231,110)")
+        self.editSectionTitleBtn.setIcon(QIcon('images/edit.png'))
+        self.editSectionTitleBtn.clicked.connect(self.showCreateSection)
+        self.index = None
+        self.sectionTitle.setFont(QFont("Century Gothic",8,QFont.Bold))
+        self.sectionTitleLayout.addWidget(self.sectionTitle)
+        self.sectionTitleLayout.addWidget(self.editSectionTitleBtn)
+
+        self.mainSectionLayout = QVBoxLayout()
+        self.mainSectionLayout.addLayout(self.sectionTitleLayout)
+        self.mainSectionLayout.addWidget(self.section)
+        self.setColor(self.setColorSection)
+        self.setLayout(self.mainSectionLayout)
+
+    def showErrorSectionTitleEmpty(self):
+        createErrorDialogBox(self,"Error","Section title can not be empty")
+
+    def showCreateSection(self):
+        self.newTitleAndDialogBox = createAddDialog(self,'Edit section title','Section name: ', 'Save',self.editTitleBtnFunc)
+
+    def getSectionTitle(self):
+        return self.sectionTitle.text()
+
+    def setIndexSection(self,index):
+        self.index = index
+
+    def editTitleBtnFunc(self):
+        self.newSectionTitle = self.newTitleAndDialogBox[0].text()
+        if self.newSectionTitle == '':
+            self.showErrorSectionTitleEmpty()
+        else:
+            self.editTitle(self.newSectionTitle)
+            self.closeEditDialogBox()
+
+    def editTitle(self,sectionTitle):
+        self.sectionTitle.setText(sectionTitle)
+
+    def closeEditDialogBox(self):
+        self.newTitleAndDialogBox[1].reject()
+
+    def setColor(self,colorName):
+        self.palette = QPalette()
+        self.setAutoFillBackground(True)
+        #self.palette.setColor(QPalette.Window, QColor(colorName))
+        self.palette.setColor(QPalette.Window, QColor('#52719F'))
+        self.setPalette(self.palette) 
