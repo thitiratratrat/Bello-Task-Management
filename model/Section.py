4fea598c843b44a2bf3ebc0a2bd5c3c79fdd1af9
thitirat1 <thitirat@code-app.com>
2020-04-08 21:56:08 +0700

    client edit section title

--- model/Section.py
+++ model/Section.py
@@ -1,16 +1,21 @@
 from typing import List
 
+
 class Section:
-    def __init__(self, title: str, tasks={}):
+    def __init__(self, title, id, tasks={}):
+        self.__id = id
         self.__title = title
         self.__tasks = tasks
+
+    def getId(self):
+        return self.__id
 
     def getTitle(self) -> str:
         return self.__title
 
     def getTasks(self):
         return self.__tasks
-    
+
     def editTitle(self, title):
         self.__title = title
 
@@ -21,10 +26,7 @@ class Section:
         self.__tasks.pop(taskId, None)
 
     def editTaskTitle(self, taskId, newTitle):
-        task = self.__tasks[taskId] 
-        
+        task = self.__tasks[taskId]
+
         task.editTaskTitle(newTitle)
-            
-
-
 
