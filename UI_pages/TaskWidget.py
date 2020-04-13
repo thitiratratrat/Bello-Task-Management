457fe32911a8f754b98d2df6b6adfb4c24e6c6a8
candyccyn <61090004@kmitl.ac.th>
2020-04-13 13:41:58 +0700

    add deleteSection

--- UI_pages/TaskWidget.py
+++ UI_pages/TaskWidget.py
@@ -4,11 +4,6 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 
 
-class Section(QListWidget): # addtask is here
-    def __init__(self,parent= None):
-        super(Section, self).__init__(parent)
-        self.setFixedSize(200, 420)
-
 class TaskWidget(QWidget):
     def __init__(self):
         QWidget.__init__(self, parent)
